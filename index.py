4c349f0b55fa120636ee0ff92fe118881ad6b34d
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-07-16 13:29:18 +0300

    Shows how to give hints of the potential word

--- index.py
+++ index.py
@@ -4,14 +4,15 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(f'Psst, the solution is {chosen_word}.')
 display = []
-for letter in chosen_word:
+word_length = len(chosen_word)
+for _ in range(word_length):
     display += "_"
     print(display)
 
 
 guess = input("Guess a letter:").lower()
-for position in range(len(chosen_word)):
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+print(display)
