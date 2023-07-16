50e459b2ff3dda908f94620b96aaf5c62bebb8f2
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-07-16 12:57:30 +0300

    Shows how to create a hangman game using for loop

---
+++ index.py
@@ -0,0 +1,17 @@
+#Creates the hangman game
+import random
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Psst, the solution is {chosen_word}.')
+display = []
+for letter in chosen_word:
+    display += "_"
+    print(display)
+
+
+guess = input("Guess a letter:").lower()
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
